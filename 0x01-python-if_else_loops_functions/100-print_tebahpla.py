d752b0736d0e2f95a88b2f524cb12dd5f68f3e8c
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-10-31 14:50:20 -0700

    python 0x001

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,11 +2,13 @@
 i = 122
 j = 89
 k = 1
+swp = i
 while k <= 26:
     if k % 2 == 1:
-        print(chr(i), end="")
+        swp = i
         i = i - 2
     else:
-        print(chr(j), end="")
+        swp = j
         j = j - 2
+    print(chr(swp), end="")
     k = k + 1
