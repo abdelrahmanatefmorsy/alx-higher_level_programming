6a91f3a6fc08eb454a061e104111f6978044d723
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-11-02 15:04:53 -0700

    hello

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    import hidden_4
-    for i in range(dir(hidden_4)):
+    import hidden_4 as me
+    for i in range(dir(me)):
         if i[0:2] != "__":
             print(i)
