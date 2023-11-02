e29538ecc3589718e20cc7b9df4baaf84434fc29
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-11-02 14:17:18 -0700

    hello

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     if number == 0:
         print("{} arguments.".format(number))
+    elif number == 1:
+        print("{} argument".format(number))
     else:
         print("{} arguments:".format(number))
     for i in range(0, number):
