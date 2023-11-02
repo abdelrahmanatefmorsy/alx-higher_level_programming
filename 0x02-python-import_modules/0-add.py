05cfab50d897c83d1d1573ef9154d6f1e0e7566d
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-11-02 13:18:22 -0700

    hello

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0.py import add
+    from add_0 import add
     a = 1
     b = 2
-    x = add(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, x))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
