9f53a3ce44ac8733f994d0f9a131b3e37301124c
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-11-05 14:04:35 -0800

    lists

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx >= len(my_list) or idx < 0:
+    x = len(my_list)
+    if idx >= x or idx < 0:
         return
     return(my_list[idx])
