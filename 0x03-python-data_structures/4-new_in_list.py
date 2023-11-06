6b21f77bc88a29f5664a47663247f7db9689bbf2
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-11-06 00:49:41 -0800

    lists

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    b = my_list
+def new_in_list(my_list, idx, element):
+    b = my_list[:]
     x = len(my_list)
     if idx >= x or idx < 0:
         return (my_list)
