ca72fe99814cc65a74ebaeeaea5c77b9be1dd155
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-11-06 01:31:37 -0800

    lists

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    x = len(matrix)
+    for i in range(x):
+        y = len(matrix[i])
+        for j in range(y):
+            end = " "
+            if j == y - 1:
+                end = ""
+            print("{}{}".format(matrix[i][j], end), end="")
+        print("")
