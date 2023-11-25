22ec3f493ca1f1692a6f676ec5e778d26b8bc136
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2023-11-25 05:29:18 -0800

    hello

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" square module. """
+
+
+class Square:
+    """Empty class that defines a square."""
+    def __init__(self, size=0):
+        """intialize values"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter method"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """setter method"""
+        if type(size) != int:
+            raise ValueError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return value"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end="")
+                print()
