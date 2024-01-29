9f91bd00c066878e06c8e44ea03e25777e729686
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-01-29 14:19:43 -0800

    hello

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,20 +3,30 @@
 
 
 class Rectangle:
+    """ class rectangle"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """
+        width
+        """
         return self.___width
 
     @property
     def height(self):
+        """
+        height
+        """
         return self.___height
 
     @width.setter
     def width(self, value):
+        """
+        width setter
+        """
         if type(value) != int:
             raise ValueError("width must be an integer")
         if value < 0:
@@ -25,6 +35,9 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """
+        height setter
+        """
         if type(value) != int:
             raise ValueError("height must be an integer")
         if value < 0:
