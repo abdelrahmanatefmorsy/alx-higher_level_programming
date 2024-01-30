4945118bb385345b29e6cea34a95368353d0aa88
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-01-30 07:38:37 -0800

    hello

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """ class rectangle"""
     def __init__(self, width=0, height=0):
         """ Instantiation with optional width and height"""
-        self.___width = width
-        self.___height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
