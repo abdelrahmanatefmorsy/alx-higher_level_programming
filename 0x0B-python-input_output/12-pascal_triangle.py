bec0234f223d04155a924b81cc094a8a5cc985ef
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-02-06 13:13:02 -0800

    hello

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    def pascal_triangle(n):
+        mylist = []
+        for i in range(1, n + 1):
+            for j in range(1, i + 1):
+                mylist.append(j)
+        return mylist
