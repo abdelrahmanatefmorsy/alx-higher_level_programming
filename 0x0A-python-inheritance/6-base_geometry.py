44cedaccf0f2981c8137d43443ef916875298f70
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-02-06 05:57:30 -0800

    hello

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" module line """
+
+
+class BaseGeometry:
+    """define a class"""
+
+    pass
+
+    def area(self):
+        """ check area """
+
+        TypeError("area() is not implemented")
