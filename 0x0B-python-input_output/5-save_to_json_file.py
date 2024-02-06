b7495a92eeb6bd65c74ecf7f83302748f4ee9370
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-02-06 12:23:09 -0800

    hello

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -9,4 +9,4 @@ def save_to_json_file(my_obj, filename):
     """ json return dictionary """
 
     with open(filename, "w", encoding="UTF-8") as file:
-        file.write(json.dumbs(my_obj))
+        file.write(json.dumps(my_obj))
