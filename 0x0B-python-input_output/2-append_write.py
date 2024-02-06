6782325620efb2b4290ee662967e4d58083d648d
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-02-06 11:43:54 -0800

    hello

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" module """
+
+
+def append_write(filename="", text=""):
+    """ open file with with """
+
+    with open(filename, "a", encoding="UTF-8") as file:
+        """ print file """
+
+        return file.append(text)
