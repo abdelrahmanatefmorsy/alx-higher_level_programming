dbb206e416e77128ac10dcac3e3d891379c7f0ed
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-02-07 11:15:25 -0800

    hello

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+"""
+===================================
+module with class square
+===================================
+"""
+
+
+class Square(Rectangle):
+    """square class that inherits from BaseGeometry"""
+
+    def __init__(self, size):
+        """intialize"""
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ area define """
+
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
