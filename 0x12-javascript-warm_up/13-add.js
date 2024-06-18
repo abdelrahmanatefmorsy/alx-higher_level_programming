9d2efaa879ecf840b81ba453bf47a064dc8fb886
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-06-18 15:15:20 -0700

    hello

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-function add (a, b) {
-  if (isNaN(a) || isNaN(b)) { console.log('NaN'); } else { console.log(Number(a) + Number(b)); }
-}
-module.exports = add;
+exports.add = (a, b) => a + b;
