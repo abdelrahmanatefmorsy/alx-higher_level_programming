736ab33ffb6e7f7844618cbb63f3d9743288397c
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-06-18 14:38:35 -0700

    hello

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const varone = process.argv[2];
 const factorial = (a) => {
-	a = parseInt(a);
+  a = parseInt(a);
   if (a === 1 || isNaN(a)) { return 1; }
   return factorial(a - 1) * a;
 };
