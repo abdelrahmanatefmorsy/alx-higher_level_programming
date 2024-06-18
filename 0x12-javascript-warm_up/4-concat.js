6d70a792579bf3b21cb4ad1ea0232360c54c1928
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-06-18 13:31:33 -0700

    hello

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-console.log(process.argv[2], ' is ', process.argv[3]);
+let varone = process.argv[2];
+let vartwo = process.argv[3];
+if (varone === undefined) { varone = 'undefined'; }
+if (vartwo === undefined) { vartwo = 'undefined'; }
+console.log(varone, ' is ', vartwo);
