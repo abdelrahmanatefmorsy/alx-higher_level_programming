e1a0f16228138b81db77a64458b13ff9a11351b5
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-06-19 13:44:44 -0700

    hello

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,11 +9,11 @@ module.exports = class Square extends require('./4-rectangle.js') {
     let i = 0;
     let j = 0;
     let str = '';
-    while (i < this.size) {
+    while (i < this.width) {
       str += c;
       i++;
     }
-    while (j < this.size) {
+    while (j < this.height) {
       console.log(str);
       j++;
     }
