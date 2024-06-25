95f8bb08d8e8137658c99f00af5b9d26eb8c5a5f
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-06-25 21:27:50 +0300

    hello

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -67,3 +67,10 @@ class Rectangle(Base):
     def area(self):
         """return  area """
         return self.__width * self.__height
+    
+    def display(self):
+        """display"""
+        for i in range(0,self.__height):
+            for j in range(0,self.__width):
+                print("#",end="")
+            print("")
