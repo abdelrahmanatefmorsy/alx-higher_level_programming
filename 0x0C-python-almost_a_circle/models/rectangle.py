5d528582f634a59a2712b767841b31f4427d0998
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-06-25 20:51:12 +0300

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,12 +3,12 @@
 from .base import Base
 
 class Rectangle(Base):
-    """class Base"""
-    __nb_objects=0
+    """class Rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Instantiation"""
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
+        self._id=id
         super(id)
