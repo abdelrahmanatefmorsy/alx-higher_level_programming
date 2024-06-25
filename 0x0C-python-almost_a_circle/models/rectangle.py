71e129d4f960a41782d85298fd90f3956c1f8a6a
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-06-25 21:12:56 +0300

    test

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -26,9 +26,20 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """setter"""
+        self.validate_integer(self,"width",width,False)
         self.__width = width
 
     @height.setter
     def height(self, height):
         """setter"""
+        self.validate_integer(self,"height",height,False)
         self.__height = height
+
+    def validate_integer(self, name, value, eq=True):
+        '''Method for validating the value.'''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
