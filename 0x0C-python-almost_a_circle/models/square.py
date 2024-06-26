498555bd34bed431a5ad027a27cd2dad831fcb6a
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-06-26 07:14:13 +0300

    hello world

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""empty class Square  that defines a rectangle"""
+from .rectangle import Rectangle
+
+
+class Square (Rectangle):
+    """class Square """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Instantiation"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """str"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width, self.height)
