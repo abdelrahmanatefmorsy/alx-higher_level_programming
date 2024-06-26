99666270948b10b54584b7b8f344ff545a34b9da
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-06-26 07:16:22 +0300

    hello world

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,4 +11,4 @@ class Square (Rectangle):
 
     def __str__(self):
         """str"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width, self.height)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
